6d7350c919be2259226a102b13af38d043df8e17
Negar <negawr24a@gmail.com>
2023-07-16 20:38:54 +0330

    login/logout page

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import TaskList , TaskDetail ,TaskCreate , TaskUpdate , TaskDelete , CostomeLoginView
+from .views import TaskList , TaskDetail ,TaskCreate , TaskUpdate , TaskDelete , CustomeLoginView
+from django.contrib.auth.views import LogoutView
+
+
 
 urlpatterns = [
 
-    path('login/', CostomeLoginView.as_view() , name='login'),
+    path('login/', CustomeLoginView.as_view() , name='login'),
+    path('logout/', LogoutView.as_view(next_page='login') , name='logout'),
     path('', TaskList.as_view() , name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view() , name='task'),
     path('task_create/', TaskCreate.as_view() , name='task_create'),
