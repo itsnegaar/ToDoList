d5bef874a38d95356d11dae0331403c200936872
Negar <negawr24a@gmail.com>
2023-07-17 21:14:52 +0330

    register

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList , TaskDetail ,TaskCreate , TaskUpdate , TaskDelete , CustomeLoginView
+from .views import TaskList , TaskDetail ,TaskCreate , TaskUpdate , TaskDelete , CustomeLoginView , RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
@@ -8,6 +8,7 @@ urlpatterns = [
 
     path('login/', CustomeLoginView.as_view() , name='login'),
     path('logout/', LogoutView.as_view(next_page='login') , name='logout'),
+    path('register/',RegisterPage.as_view(), name = 'register'),
     path('', TaskList.as_view() , name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view() , name='task'),
     path('task_create/', TaskCreate.as_view() , name='task_create'),
